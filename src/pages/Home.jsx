486b47ac9665f6dcfc76bc662026f5fd3d48f460
fahmiprasetio <nfahmi086@gmail.com>
2025-07-03 07:54:43 +0700

    banyak lah pokoknya

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground"
+
+export const Home = () => {
+
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+            {/* theme toggle */}
+            <ThemeToggle />
+            {/* background effects */}
+            <StarBackground />
+
+            {/* navbar */}
+
+        </div>
+    );
+};
