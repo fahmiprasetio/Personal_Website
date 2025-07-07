5fb872aa590ebdc912a6c9c95061571b8d5a9862
mendoan <nfahmi086@gmail.com>
2025-07-08 00:16:34 +0700

    Update AboutSection.jsx
    link cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -51,6 +51,7 @@ export const AboutSection = () => {
               <a
                 href="https://drive.google.com/file/d/1mgihQYpsAjRWzaXltGCQP98ayHM2K6l2/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+                target="_blank"
               >
                 Download My CV
               </a>
