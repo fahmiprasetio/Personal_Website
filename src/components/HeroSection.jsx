6362e9cdc2e3baab56a78b2f8fd8ff14f6eb68ae
fahmiprasetio <nfahmi086@gmail.com>
2025-07-08 11:05:38 +0700

    adjut layouting hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ export const HeroSection = () => {
     >
       <div className="container max-w-4xl mx-auto text-center z-10 ">
         <div className="space-y-6">
-          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+          <h1 className="text-2xl md:text-6xl font-bold tracking-tight mb-3 md:mb-5">
             <span className="opacity-0 animate-fade-in">
               Hello There! I'm <br />
               Naufal{" "}
@@ -20,16 +20,16 @@ export const HeroSection = () => {
               Prasetio
             </span>
           </h1>
-          <p className="text-md md:text-xl lg:text-xl text-muted-foreground max-w-6xl mx-auto opacity-0 animate-fade-in-delay-3">
-            “I transform complex problems into simple, meaningful, and effective
+          <p className="text-xs md:text-xl lg:text-xl text-muted-foreground max-w-6xl mx-auto opacity-0 animate-fade-in-delay-3">
+            I transform complex problems into simple, meaningful, and effective
             web solutions, delivering clarity and impact through thoughtful,
-            user-centered design.”
+            user-centered design.
           </p>
 
-          <div className="pt-4 opacity-0 animate-fade-in-delay-4 flex items-center justify-center gap-3">
+          <div className="pt-4 opacity-0 animate-fade-in-delay-4 flex items-center justify-center gap-3 -mt-4 md:mt-6">
             <a
               href="#project"
-              className="cosmic-button rounded-xl py-3 hover:scale-102 inline-block w-60 sm:text-md"
+              className="cosmic-button w-60 max-w-xs !text-md sm:!text-xl rounded-xl py-2.5 hover:scale-102 inline-block text-center"
             >
               View My Project
             </a>
@@ -37,7 +37,7 @@ export const HeroSection = () => {
               href="https://github.com/fahmiprasetio"
               target="_blank"
               rel="noopener noreferrer"
-              className=" cosmic-button transition duration-300 p-2 rounded-xl border-white"
+              className=" cosmic-button transition duration-300 p-1.5 sm:p-2 rounded-xl border-white "
             >
               <Github className="w-8 h-8" />
             </a>
@@ -45,7 +45,7 @@ export const HeroSection = () => {
               href="https://linkedin.com/in/fahmiprasetio"
               target="_blank"
               rel="noopener noreferrer"
-              className=" cosmic-button transition duration-300 p-2 rounded-xl border-white"
+              className=" cosmic-button transition duration-300 p-1.5 sm:p-2 rounded-xl border-white"
             >
               <Linkedin className="w-8 h-8" />
             </a>
